7f49b72b35442cb445bcf671b6dbfc99b23129c8
Superhero23ufo <dbehzadx@IMacPro>
2023-09-28 19:38:09 -0700

    the button is working

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
 console.log("Hello World")
+
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+
+function closePopup() {
+    popup.classList.remove("open-popup");
+}
